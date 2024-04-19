26c19c11e175f4a32b235ca22ae6081a8cc099ff
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-04-19 09:20:48 -0700

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# A puppet installing flask v2.1.0 which is a package from pip3.
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
 # Am also specifying the version of flask to install using the ensure attribute
 package { 'flask':
   ensure   => '2.1.0',
