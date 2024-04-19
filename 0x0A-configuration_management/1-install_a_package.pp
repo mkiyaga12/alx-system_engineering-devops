3d060ce4d36e6dfd8b3f8bd10a7945d15e8397ab
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-04-19 10:12:44 -0700

    installing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # A puppet manifest installing flask v2.1.0 which is a package from pip3.
 package { 'flask':
-ensure   => '2.1.0',
-provider => pip3,
+	ensure   => '2.1.0',
+	provider => pip3,
 }
+
