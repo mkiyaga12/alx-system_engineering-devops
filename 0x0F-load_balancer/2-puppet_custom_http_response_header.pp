e5e6e78834195607eb721e38dba058496b83b38c
mkiyaga12 <kiyagamuzamiru@gmail.com>
2024-05-06 11:39:53 -0700

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+# Using Puppet to automate a custom HTTP header response
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+  require => Exec['update'],
+}
+
+file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  line  => 'add_header X-Served-By $hostname;',
+  match => '^http {',
+}
+
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  hasstatus => true,
+  require   => [Package['nginx'], File_line['http_header']],
+}
